b751f0c858313bf09bfe86af8cbda3530d744dd9
Nicolas B <nicolasboulenc@gmail.com>
2025-10-04 11:48:43 +0100

    yet another commit

--- main.go
+++ main.go
@@ -7,22 +7,34 @@ import (
 )
 
 const (
-	PLAYER_SPEED = 200
+	PLAYER_SPEED = 200 // pixels per second
 )
 
 type Player struct {
-	pos   rl.Vector2
-	dir   rl.Vector2
-	speed float32
+	pos            rl.Vector2
+	dir            rl.Vector2
+	speed          float32 // pixel per second
+	fire_rate      int     // shots per second
+	fire_prev_time float64
+	fire_vel       float32
+	is_firing      bool
+}
+
+type Projectile struct {
+	enabled bool
+	pos     rl.Vector2
+	dir     rl.Vector2
 }
 
 type State struct {
 	in_menu   bool
 	is_paused bool
+	prev_time float64
 }
 
 var player Player = Player{pos: rl.Vector2{X: 100, Y: 100}, dir: rl.Vector2{X: 0, Y: 0}, speed: PLAYER_SPEED}
 var game_state State = State{in_menu: true, is_paused: true}
+var projectiles = make([]Projectile, 0, 10)
 
 func main() {
 	screenWidth := int32(400)
@@ -33,28 +45,50 @@ func main() {
 	rl.SetWindowPosition(10, 10)
 
 	buffer := rl.LoadRenderTexture(screenWidth, screenHeight)
-
-	// NOTE: Textures MUST be loaded after Window initialization (OpenGL context is required)
 	texture := rl.LoadTexture("images/UK_Spitfire.png") // Texture loading
 
 	rl.SetTargetFPS(60)
 
+	game_state.prev_time = rl.GetTime()
+	player.fire_prev_time = game_state.prev_time
+
 	for !rl.WindowShouldClose() {
 
-		dtime := rl.GetFrameTime()
+		ntime := rl.GetTime()
+		dtime := ntime - game_state.prev_time
+		game_state.prev_time = ntime
 
+		// inputs
 		process_inputs()
-		player.pos.X += player.dir.X * player.speed * dtime
-		player.pos.Y += player.dir.Y * player.speed * dtime
+		player.pos.X += player.dir.X * player.speed * float32(dtime)
+		player.pos.Y += player.dir.Y * player.speed * float32(dtime)
 
+		// logic
+		if player.is_firing && ntime-player.fire_prev_time >= 1/float64(player.fire_rate) {
+			projectiles = append(projectiles, Projectile{enabled: true, pos: rl.Vector2{X: player.pos.X, Y: player.pos.Y}})
+			player.fire_prev_time = ntime
+		}
+
+		for i := 0; i < len(projectiles); i++ {
+		}
+
+		// draw to texture
 		rl.BeginTextureMode(buffer)
 		rl.ClearBackground(rl.RayWhite)
+
 		rl.DrawTexture(texture, int32(player.pos.X)-texture.Width/2, int32(player.pos.Y)-texture.Height/2, rl.White)
+
+		for i := 0; i < len(projectiles); i++ {
+			rl.DrawRectangle(int32(projectiles[i].pos.X), int32(projectiles[i].pos.Y), 10, 10, rl.Black)
+		}
+
 		rl.EndTextureMode()
 
+		// draw to screen
 		rl.BeginDrawing()
 		rl.DrawTextureRec(buffer.Texture, rl.Rectangle{X: 0, Y: 0, Width: float32(buffer.Texture.Width), Height: float32(-buffer.Texture.Height)}, rl.Vector2{X: 0, Y: 0}, rl.White)
-		rl.DrawText("this IS a texture!", 360, 370, 10, rl.Gray)
+		rl.DrawText("", 360, 370, 10, rl.Gray)
+		rl.DrawText(fmt.Sprintf("this IS a texture! %d", player.is_firing), 10, 10, 10, rl.Gray)
 		rl.EndDrawing()
 	}
 
@@ -80,78 +114,9 @@ func process_inputs() {
 	if rl.IsKeyDown(rl.KeyDown) || rl.IsKeyDown(rl.KeyS) {
 		player.dir.Y += 0.8
 	}
-	player.dir = rl.Vector2Normalize(player.dir)
-
-	// gamepad
-	var gamepad int32 = 0 // which gamepad to display
-	// fmt.Println(rl.GetGamepadName(gamepad))
-	if rl.IsGamepadAvailable(gamepad) {
-		rl.DrawText(fmt.Sprintf("GP1: %s", rl.GetGamepadName(gamepad)), 10, 10, 10, rl.Black)
-
-		// Draw buttons: xbox home
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonMiddle) {
-			rl.DrawText("Button: Middle", 10, 25, 10, rl.Black)
-		}
-
-		// Draw buttons: basic
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonMiddleRight) {
-			rl.DrawText("Button: START", 10, 25, 10, rl.Black)
-		}
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonMiddleLeft) {
-			rl.DrawText("Button: BACK", 10, 25, 10, rl.Black)
-		}
-
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonRightFaceLeft) {
-			rl.DrawText("Button: X", 10, 25, 10, rl.Black)
-		}
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonRightFaceDown) {
-			rl.DrawText("Button: A", 10, 25, 10, rl.Black)
-		}
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonRightFaceRight) {
-			rl.DrawText("Button: B", 10, 25, 10, rl.Black)
-		}
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonRightFaceUp) {
-			rl.DrawText("Button: Y", 10, 25, 10, rl.Black)
-		}
-
-		// Draw buttons: d-pad
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonLeftFaceUp) {
-			rl.DrawText("Button: D-UP", 10, 25, 10, rl.Black)
-		}
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonLeftFaceDown) {
-			rl.DrawText("Button: D-DOWN", 10, 25, 10, rl.Black)
-		}
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonLeftFaceLeft) {
-			rl.DrawText("Button: D-LEFT", 10, 25, 10, rl.Black)
-		}
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonLeftFaceRight) {
-			rl.DrawText("Button: D-RIGHT", 10, 25, 10, rl.Black)
-		}
-
-		// Draw buttons: left-right back
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonLeftTrigger1) {
-			rl.DrawText("Button: Shoulder-L", 10, 25, 10, rl.Black)
-		}
-		if rl.IsGamepadButtonDown(gamepad, rl.GamepadButtonRightTrigger1) {
-			rl.DrawText("Button: Shoulder-R", 10, 25, 10, rl.Black)
-		}
-
-		// Draw axis: left joystick
-		// rl.DrawCircle(259, 152, 39, rl.Black)
-		// rl.DrawCircle(259, 152, 34, rl.LightGray)
-		// rl.DrawCircle(int32(259+(rl.GetGamepadAxisMovement(gamepad, rl.GamepadAxisLeftX)*20)),
-		// 	int32(152-(rl.GetGamepadAxisMovement(gamepad, rl.GamepadAxisLeftY)*20)), 25, rl.Black)
-
-		// Draw axis: right joystick
-		// rl.DrawCircle(461, 237, 38, rl.Black)
-		// rl.DrawCircle(461, 237, 33, rl.LightGray)
-		// rl.DrawCircle(int32(461+(rl.GetGamepadAxisMovement(gamepad, rl.GamepadAxisRightX)*20)),
-		// 	int32(237-(rl.GetGamepadAxisMovement(gamepad, rl.GamepadAxisRightY)*20)), 25, rl.Black)
-
-		// Draw axis: left-right triggers
-		// rl.DrawRectangle(170, 30, 15, 70, rl.Gray)
-		// rl.DrawRectangle(604, 30, 15, 70, rl.Gray)
-		// rl.DrawRectangle(170, 30, 15, int32(((1.0+rl.GetGamepadAxisMovement(gamepad, rl.GamepadAxisLeftTrigger))/2.0)*70), rl.Red)
-		// rl.DrawRectangle(604, 30, 15, int32(((1.0+rl.GetGamepadAxisMovement(gamepad, rl.GamepadAxisRightTrigger))/2.0)*70), rl.Red)
+	if rl.IsKeyDown(rl.KeySpace) {
+		player.is_firing = true
 	}
+
+	player.dir = rl.Vector2Normalize(player.dir)
 }
