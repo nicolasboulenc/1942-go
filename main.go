f24c7d77eb1e46c4943b1e0f5f241366b3e30e58
Nicolas B <nicolasboulenc@gmail.com>
2025-10-05 20:13:36 +0100

    yet anotehr commit

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"log"
 	"os"
-	"reflect"
 	"strconv"
 	"time"
 
@@ -12,21 +11,24 @@ import (
 )
 
 const (
-	BONUS_TYPE_MORE     = 1
-	BONUS_TYPE_RATE     = 2
-	BONUS_TYPE_SPEED    = 3
-	BONUS_MVT_OSCILLATE = 1
-	BONUS_MVT_STRAIGHT  = 2
+	// movement types
+	MOVEMENT_TYPE_OSCILLATE = 1
+	MOVEMENT_TYPE_STRAIGHT  = 2
+	// weapon fire types
+	FIRE_TYPE_ALTERNATE = 1
+	FIRE_TYPE_DUAL      = 2
+	FIRE_TYPE_QUAD      = 3
+	// debug stuff
 	DEBUG_CONF_INTERVAL = 0.5 // check every x seconds for config file change
 )
 
 type Player struct {
-	pos            rl.Vector2
-	dir            rl.Vector2
-	speed          float32 // pixel per second
-	fire_rate      int     // shots per second
-	fire_prev_time float64
-	is_firing      bool
+	pos                rl.Vector2
+	dir                rl.Vector2
+	velocity_modifier  float32 // 1.2x
+	fire_rate_modifier float64 // 2x
+	is_firing          bool
+	weapons            []Weapon
 }
 
 type Config struct {
@@ -36,22 +38,19 @@ type Config struct {
 	bonus_velocity      float32
 }
 
-type Bonus struct {
-	type_ int
-	pos   rl.Vector2
-	speed float32
-}
-
 type Projectile struct {
-	enabled bool
-	pos     rl.Vector2
-	dir     rl.Vector2
+	enabled  bool
+	pos      rl.Vector2
+	dir      rl.Vector2
+	velocity float32
 }
 
 type GameState struct {
-	in_menu   bool
-	is_paused bool
-	prev_time float64
+	screen_width  int32
+	screen_height int32
+	in_menu       bool
+	is_paused     bool
+	prev_time     float64
 }
 
 type Debug struct {
@@ -59,33 +58,34 @@ type Debug struct {
 	conf_check_prev_time float64   // last time we checked
 }
 
-var player Player = Player{pos: rl.Vector2{X: 100, Y: 100}, dir: rl.Vector2{X: 0, Y: 0}, speed: config.player_velocity, fire_rate: 16}
-var game_state GameState = GameState{in_menu: true, is_paused: true}
-var projectiles = make([]Projectile, 10)
-var bonus Bonus
+type Weapon struct {
+	projectile_velocity float32 // 1.2x
+	projectile_damage   float32
+	state               int
+	fire_type           int     // single, burst, auto, etc
+	fire_rate           float64 // 2x
+	fire_prev_time      float64
+	is_firing           bool
+}
+
+var player Player
+var game_state GameState
+var projectiles []Projectile
 var debug Debug
 var config Config
 
 func main() {
-	screenWidth := int32(400)
-	screenHeight := int32(400)
 
-	debug.conf_prev_time = config_check()
-	config_load()
+	game_init()
 
 	rl.SetConfigFlags(rl.FlagWindowHighdpi)
-	rl.InitWindow(screenWidth, screenHeight, "1942-go")
+	rl.InitWindow(game_state.screen_width, game_state.screen_height, "1942-go")
 	rl.SetWindowPosition(10, 10)
 
-	buffer := rl.LoadRenderTexture(screenWidth, screenHeight)
+	buffer := rl.LoadRenderTexture(game_state.screen_width, game_state.screen_height)
 	texture := rl.LoadTexture("images/UK_Spitfire.png") // Texture loading
 
 	rl.SetTargetFPS(60)
-
-	game_state.prev_time = rl.GetTime()
-	player.fire_prev_time = game_state.prev_time
-
-	bonus = Bonus{type_: BONUS_TYPE_MORE, pos: rl.Vector2{}, speed: config.bonus_velocity}
 
 	for !rl.WindowShouldClose() {
 
@@ -95,14 +95,29 @@ func main() {
 
 		// inputs
 		process_inputs()
-		player.pos.X += player.dir.X * player.speed * float32(dtime)
-		player.pos.Y += player.dir.Y * player.speed * float32(dtime)
+		player.pos.X += player.dir.X * config.player_velocity * player.velocity_modifier * float32(dtime)
+		player.pos.Y += player.dir.Y * config.player_velocity * player.velocity_modifier * float32(dtime)
 
 		// logic
-		if player.is_firing && ntime-player.fire_prev_time >= 1/float64(player.fire_rate) {
-			projectile := Projectile{enabled: true, pos: rl.Vector2{X: player.pos.X, Y: player.pos.Y}}
-			projectiles = append(projectiles, projectile)
-			player.fire_prev_time = ntime
+		if player.is_firing {
+			for i := 0; i < len(player.weapons); i++ {
+				weapon := &player.weapons[i]
+				if ntime-weapon.fire_prev_time >= 1/weapon.fire_rate {
+					projectile := player_weapon_fire(weapon, player)
+					projectiles = append(projectiles, projectile)
+				}
+			}
+		}
+
+		for i := 0; i < len(projectiles); i++ {
+
+			projectiles[i].pos.Y -= config.projectile_velocity * float32(dtime)
+
+			proj_rect := rl.Rectangle{X: projectiles[i].pos.X, Y: projectiles[i].pos.Y, Width: 1, Height: 10}
+			view_rect := rl.Rectangle{X: 0, Y: 0, Width: float32(game_state.screen_width), Height: float32(game_state.screen_height)}
+			if !rl.CheckCollisionRecs(proj_rect, view_rect) {
+				projectiles[i].enabled = false
+			}
 		}
 
 		if debug.conf_check_prev_time+DEBUG_CONF_INTERVAL > ntime {
@@ -111,17 +126,6 @@ func main() {
 				config_load()
 			}
 			debug.conf_check_prev_time = ntime
-		}
-
-		for i := 0; i < len(projectiles); i++ {
-
-			projectiles[i].pos.Y -= config.projectile_velocity * float32(dtime)
-
-			proj_rect := rl.Rectangle{X: projectiles[i].pos.X, Y: projectiles[i].pos.Y, Width: 1, Height: 10}
-			view_rect := rl.Rectangle{X: 0, Y: 0, Width: float32(screenWidth), Height: float32(screenHeight)}
-			if !rl.CheckCollisionRecs(proj_rect, view_rect) {
-				projectiles[i].enabled = false
-			}
 		}
 
 		// draw to texture
@@ -149,6 +153,55 @@ func main() {
 	rl.CloseWindow()
 }
 
+func game_init() {
+
+	player = Player{pos: rl.Vector2{X: 100, Y: 100}, dir: rl.Vector2{X: 0, Y: 0}, velocity_modifier: 1, fire_rate_modifier: 1, is_firing: false, weapons: make([]Weapon, 0, 3)}
+	game_state = GameState{in_menu: true, is_paused: true, screen_width: 640, screen_height: 480}
+	projectiles = make([]Projectile, 0, 10)
+
+	weapon := Weapon{projectile_velocity: 400, projectile_damage: 2, state: 0, fire_type: FIRE_TYPE_ALTERNATE, fire_rate: 16, fire_prev_time: 0, is_firing: false}
+	player_weapon_add(weapon)
+
+	debug.conf_prev_time = config_check()
+	config_load()
+
+	game_state.prev_time = rl.GetTime()
+}
+
+func player_weapon_add(weapon Weapon) {
+
+	// check the weapon doesn already exist
+	for i := 0; i < len(player.weapons); i++ {
+		if player.weapons[i].fire_type == weapon.fire_type {
+			// remove that weapon
+			player.weapons = append(player.weapons[:i], player.weapons[i+1:]...)
+			break
+		}
+	}
+
+	player.weapons = append(player.weapons, weapon)
+}
+
+func player_weapon_fire(weapon *Weapon, player Player) Projectile {
+
+	var origin rl.Vector2
+	origin.Y = player.pos.Y
+
+	switch weapon.fire_type {
+	case FIRE_TYPE_ALTERNATE:
+		if weapon.state == 0 {
+			weapon.state = 1
+			origin.X = player.pos.X - 10
+		} else {
+			weapon.state = 0
+			origin.X = player.pos.X + 10
+		}
+		weapon.fire_prev_time = rl.GetTime()
+		return Projectile{enabled: true, pos: origin, velocity: weapon.projectile_velocity}
+	}
+	return Projectile{enabled: false}
+}
+
 func process_inputs() {
 
 	player.dir = rl.Vector2Zero()
@@ -170,7 +223,13 @@ func process_inputs() {
 	if rl.IsKeyDown(rl.KeySpace) {
 		player.is_firing = true
 	}
-
+	if rl.IsKeyDown(rl.KeySpace) {
+		player.is_firing = true
+	}
+	if rl.IsKeyDown(rl.KeyU) {
+		weapon := Weapon{projectile_velocity: 400, projectile_damage: 2, fire_type: FIRE_TYPE_ALTERNATE, fire_rate: 400, fire_prev_time: 0, is_firing: false}
+		player.weapons = append(player.weapons, weapon)
+	}
 	player.dir = rl.Vector2Normalize(player.dir)
 }
 
@@ -192,22 +251,27 @@ func config_load() {
 	fileScanner := bufio.NewScanner(file)
 	fileScanner.Split(bufio.ScanWords)
 
-	tt := reflect.ValueOf(config)
 	field_name := ""
 	for fileScanner.Scan() {
 		if field_name == "" {
 			field_name = fileScanner.Text()
 		} else {
 			value_text := fileScanner.Text()
-			if field_name == "player_velocity" {
-				config.player_velocity, _ = strconv.ParseFloat(value_text, 32)
-			} else if field_name == "player_fire_rate" {
-				config.player_velocity, _ = strconv.ParseFloat(value_text, 32)
-			} else if field_name == "projectile_velocity" {
-				config.player_velocity, _ = strconv.ParseFloat(value_text, 32)
-			} else if field_name == "bonus_velocity" {
-				config.player_velocity, _ = strconv.ParseFloat(value_text, 32)
+			switch field_name {
+			case "player_velocity":
+				val, _ := strconv.ParseInt(value_text, 10, 32)
+				config.player_velocity = float32(val)
+			case "player_fire_rate":
+				val, _ := strconv.ParseInt(value_text, 10, 32)
+				config.player_fire_rate = int(val)
+			case "projectile_velocity":
+				val, _ := strconv.ParseInt(value_text, 10, 32)
+				config.projectile_velocity = float32(val)
+			case "bonus_velocity":
+				val, _ := strconv.ParseInt(value_text, 10, 32)
+				config.bonus_velocity = float32(val)
 			}
+			field_name = ""
 		}
 	}
 
